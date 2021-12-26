beb5b9def4765f287b30f16f3f089c4897ccee48
salsasabielar <salsa12rosyada@gmail.com>
2021-12-26 21:09:53 +0700

    2nd commit

--- tambahDataWarga_user/updateData.php
+++ tambahDataWarga_user/updateData.php
@@ -1,6 +1,6 @@
 <?php 
  
-include '../crudManageUser/config.php';
+include '../config.php';
 $id_warga = $_POST['id_warga'];
 $nik = $_POST['nik'];
 $nama = $_POST['nama'];
@@ -8,38 +8,12 @@ $alamat = $_POST['alamat'];
 $ttl = $_POST['ttl'];
 $pekerjaan = $_POST['pekerjaan'];
 $jenisKelamin = $_POST['jenisKelamin'];
-$tanggalsurvey = $_POST['tanggalsurvey'];
-$ceka=$_POST['ya'];
-$jumlaha=count($ceka)*1;
-echo $jumlaha;
 
-mysqli_query($koneksi,"UPDATE warga SET nik='$nik', nama='$nama', ttl='$ttl',pekerjaan='$pekerjaan',jenisKelamin='$jenisKelamin', tanggalsurvey='$tanggalsurvey'
- WHERE id_warga='$id_warga'");
-
-if (mysqli_query($koneksi,"DELETE FROM kriteria_warga WHERE id_warga='$id_warga'")) {
-    for($i=0; $i < $jumlaha; $i++){    
-        if(mysqli_query ($koneksi, "INSERT INTO kriteria_warga (id_kriteria, id_warga) values ($ceka[$i], $id_warga)")) {               
-        
-        } else{
-        echo mysqli_error($koneksi);
-      }    
-    
-    }
-    $result = mysqli_query($koneksi, "SELECT COUNT(*) as total FROM kriteria_warga WHERE id_warga = $id_warga");
-        $row = mysqli_fetch_array($result);
-        $count = $row['total'];
-        echo $count;
-        $status = "";
-                        if($count >= 11 ){$status='Sangat Layak';}
-                        else if($count <= 10 && $count >= 6 ){$status='Layak';}
-                        else {$status='Tidak Layak';}
-                        
-                    echo $status;
-        if(mysqli_query($koneksi,"UPDATE warga SET status='$status' WHERE id_warga='$id_warga'")){
-           header("location:tambahData.php");
-           
-        }
-
+if (mysqli_query($koneksi,"UPDATE warga SET nik='$nik', nama='$nama', alamat='$alamat', ttl='$ttl',pekerjaan='$pekerjaan',jenisKelamin='$jenisKelamin'
+WHERE id_warga='$id_warga'")) {
+  header("location:tambahData.php");
+} else{
+ echo mysqli_error($koneksi);
 }
  
 
